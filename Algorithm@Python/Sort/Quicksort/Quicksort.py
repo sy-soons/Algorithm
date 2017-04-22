7e5b875c2cbdda42d284a8a4f8a866d6022885f4
unlocker-sy <soonyoung87@gmail.com>
2017-04-23 01:02:00 +0900

    [Quick sort]  1. Linfeed가 정상적으로 반영되지 않는 문제](Quicksort_Partitioning.md)   - 매 라인의 끝에 공백 2개를 추가함.
     2. partitioning 함수 추가
      - 리스트 인덱스 에러 있음.(list assignment index out of range)
      recursive function 동작중에 인덱스값이 잘못되어 리스트의 범위를
      벗어난 원소를 참조헤서 발생하는 에러임.
      (temp_sorted 리스트에 left, right인덱스로 접근시에)

--- Algorithm@Python/Sort/Quicksort/Quicksort.py
+++ Algorithm@Python/Sort/Quicksort/Quicksort.py
@@ -1,19 +1,67 @@
 unsorted = [12,9,3,7,14,11,6,2,10,5]
-sorted = list(unsorted)
+#sorted = list(unsorted)
+
+def swap(ix, iy):
+	global unsorted
+	temp = unsorted[ix]
+	unsorted[ix] = unsorted[iy]
+	unsorted[iy] = temp
 
 def quicksort(low, high):
 	global unsorted
-	if(low < hi):
+	if(low < high):
 		#find pivot
 		p = partition(low, high)
+		print 'pivot :', p, 'value : ',unsorted[p],'|',low,'|',high
 		#pivot's left side, call quicksort(recursive function)
-		quicksort(low, p-1)
-		#pivot's left side, call quicksort(recursive function)
-		quicksort(p+1,high)
+		if(p>0):
+			quicksort(low, p-1)
+			#pivot's left side, call quicksort(recursive function)
+			quicksort(p+1,high)
 
 def partition(low, high):
 	global unsorted
 	#set pivot as rigit side value.
 	pivot = unsorted[high]
+	pivot_idx = high
+	ret_pivot = 0
+	#set left and right value
+	left = low
+	right = high-1
 	
+	#declare temp buffer
+	len_temp_sorted = high-low+1
+	temp_sorted = [0]*len_temp_sorted
 	
+	for i in range(low, high):
+		if(left == right):
+			ret_pivot = left
+			swap(left, pivot_idx)
+			break
+		if(unsorted[i] > pivot):
+			print 'In partition, right : ', right, ',i : ', i, ', len_temp :', len_temp_sorted
+			temp_sorted[right] = unsorted[i]
+			right-=1
+		elif(unsorted[i] < pivot):
+			temp_sorted[left] = unsorted[i]
+			left+=1
+	
+	return ret_pivot
+
+#quicksort(0, len(unsorted))
+quicksort(0, 9)
+
+
+#check swap function
+'''
+swap(0,9)
+for idx in range(0, len(unsorted)):
+	print idx, ' : ', unsorted[idx]
+'''
+#check for initializing list as specific length
+'''
+a = [0]*10
+
+for i in range(0,len(a)):
+	print 'a : ',a[i]
+'''
