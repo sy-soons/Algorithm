13fc02f54d5d16d1e6b450ca1c245b0dfbab46d5
unlocker-sy <soonyoung87@gmail.com>
2017-05-03 20:43:55 +0900

    [insertion sort] 1. Add insertion_sort() function. 2. (Insertion-sort-example-300px.gif) Add animation file to understand.

--- Algorithm@Python/Sort/Insertsort/Insertsort_Unlocked.py
+++ Algorithm@Python/Sort/Insertsort/Insertsort_Unlocked.py
@@ -1,9 +1,45 @@
 unsorted = [55, 07, 78, 12, 42]
-#unsorted = [07, 55, 78, 12, 42]
-print unsorted
-print 'unsorted\'s len is ',len(unsorted)
 
-#'''unsorted index''' 
+def insertion_sort(_unsorted):
+	''' key-index range'''
+	for i in range(1, len(_unsorted)):
+		#set key, unsorted's start-value
+		key = _unsorted[i]
+		j = i-1
+		
+		#regard that left side is already sorted
+		#if target is larger than key, target have to move
+		#find key's location, j-1
+		while j >= 0 and key < _unsorted[j]:
+			unsorted[j+1] = unsorted[j]
+			j = j-1
+		
+		#locate key
+		_unsorted[j+1] = key
+
+
+print '**********Before Sorting*****************'
+print unsorted
+'''
+print ' length : ', len(unsorted)
+for i in range(0, len(unsorted)):
+	print i,' : ', unsorted[i]
+'''
+print '*****************************************'
+print '\n'
+
+insertion_sort(unsorted)
+
+print '**********After Sorting******************'
+print unsorted
+'''
+print ' length : ', len(unsorted)
+for i in range(0, len(unsorted)):
+	print i,' : ', unsorted[i]
+'''
+print '*****************************************'
+
+'''
 for i in range(1, len(unsorted)):
 	#set key, unsorted's start-value
 	key = unsorted[i]
@@ -19,3 +55,4 @@ for i in range(1, len(unsorted)):
 	print 'sorted, j : ',j,' i',i
 	for p in range(0, len(unsorted)):
 		print unsorted[p],','
+'''
